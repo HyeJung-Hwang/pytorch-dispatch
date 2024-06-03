5da2bda950df1d6d60bd0242e2377474b8ac6fc1
hyejung <seathat33@gmail.com>
2024-06-03 17:12:39 +0900

    feat: forward/backprop에 대해 모듈별로 실행시간 재는 코드

--- time_measurement.py
+++ time_measurement.py
@@ -1,8 +1,11 @@
 import time
 from collections import defaultdict
+
 import torch
 from torch.utils._python_dispatch import TorchDispatchMode
 from torch.utils._pytree import tree_map
+import torchvision.models as models
+
 
 def normalize_tuple(x):
     if not isinstance(x, tuple):
@@ -11,6 +14,7 @@ def normalize_tuple(x):
 
 class LatencyMeasurementMode(TorchDispatchMode):
     def __init__(self, module = None):
+        self.latency_counts = defaultdict(lambda: defaultdict(int))
         self.parents = ['Global']
         if module is not None:
             for name, module in dict(module.named_children()).items():
@@ -67,6 +71,20 @@ class LatencyMeasurementMode(TorchDispatchMode):
 
         return PopState.apply
 
+    def __enter__(self):
+        self.latency_counts.clear()
+        super().__enter__()
+
+    def __exit__(self, *args):
+        total_latency = sum(self.latency_counts['Global'].values())
+        print(f"Total latency : {total_latency} seconds")
+        print(self.latency_counts.keys())
+        for mod in self.latency_counts.keys():
+            print(f"Module: ", mod)
+            for k,v in self.latency_counts[mod].items():
+                print(f"{k} latency : {v} seconds")
+            print()
+        super().__exit__(*args)
 
     def __torch_dispatch__(self, func, types, args=(), kwargs=None):
         kwargs = kwargs if kwargs else {}
@@ -75,24 +93,28 @@ class LatencyMeasurementMode(TorchDispatchMode):
         out = func(*args, **kwargs)
         end_time = time.time()
         latency = end_time - start_time
+        
         func_packet = func._overloadpacket
-        print(f"{func} Latency: {latency:.6f} seconds")
+        for par in self.parents:
+            self.latency_counts[par][func_packet] += latency
 
         return out
 
 if __name__ == "__main__":
-    import torchvision.models as models
 
     inp = torch.randn(1, 3, 224, 224, device='cpu')
     mod = models.resnet50()
     optimizer = torch.optim.Adam(mod.parameters(), lr=0.001)
 
     latency_counter = LatencyMeasurementMode(mod)
-    
-    optimizer.zero_grad()
-    outputs = mod(inp)
-    loss = outputs.sum()
+
     with latency_counter:
-        
+        print("=================== Forward =====================")
+        optimizer.zero_grad()
+        outputs = mod(inp)
+        loss = outputs.sum()
+
+    with latency_counter:
+        print("=================== Backward =====================")
         loss.backward()
-    optimizer.step()
+        optimizer.step()
